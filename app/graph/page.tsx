9877f66ff5815a4bd20fab0a19def482a250f430
Xiyan Moo <xiy4nm@gmail.com>
2026-08-20 18:12:37 +0800

    feat: add dedicated graph page

---
+++ app/graph/page.tsx
@@ -0,0 +1,9 @@
+import { BookGraph } from "@/components/BookGraph";
+
+export default function GraphPage() {
+    return (
+        <div style={{ height: "calc(100vh - 56px)" }}>
+            <BookGraph />
+        </div>
+    );
+}
